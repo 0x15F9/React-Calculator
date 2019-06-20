cf8efe8629658f4134c7b4ac0db0cfdbdc4b37b6
0x15F9 <isfaaq@gmail.com>
2019-06-20 12:31:50 +0400

    Dumb logic (but it works <(^^)/

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -9,50 +9,75 @@ class App extends Component {
 
   state = {
     displayValue : "0",
-    buttons: [
-      '7', '8', '9', '+',
-      '4', '5', '6', '-',
-      '1', '2' , '3', '*',
-      '.', '=', '0', '÷']
+    numPad: [
+      '7', '8', '9',
+      '4', '5', '6',
+      '1', '2', '3',
+      '0'],
+    operations: ['+', '-', '*', '/']
   }
 
-  onButtonClicked = (val) => {
+  onNumpadButtonClicked = (val) => {
+    const newVal = this.state.displayValue=='0' ? val : this.state.displayValue+val;
     this.setState(
       {
         ...this.state,
-        displayValue: this.state.displayValue+val
+        displayValue: newVal
       }
     )
   }
 
-  onDeleteButtonClicked = () => {
-    this.setState(
-      {
-        ...this.state,
-        displayValue: this.state.displayValue.substring( 0, this.state.displayValue.length-1 )
-      }
-    )
-  }
-
-  onResetButtonClicked = () => {
-    this.setState(
-      {
-        ...this.state,
-        displayValue: 0
-      }
-    )
+  onSpecialButtonClicked = (val) => {
+    switch (val) {
+      case 'del':
+        if (this.state.displayValue.length > 1) {
+          this.setState(
+            {
+              ...this.state,
+              displayValue: this.state.displayValue.substring( 0, this.state.displayValue.length-1 )
+            }
+          );
+        } else {
+          this.onSpecialButtonClicked('AC');
+        }
+        break;
+        case 'AC':
+          this.setState(
+            {
+              ...this.state,
+              displayValue: 0
+            }
+          );
+          break;
+      case '=':
+        this.setState(
+          {
+            ...this.state,
+            displayValue: eval(this.state.displayValue)
+          }
+        );
+        break;
+      default:
+        break;
+    }
   }
 
   render() {
     return (
       <div className="App">
         <DisplayBar displayValue={ this.state.displayValue } />
-        <Button val='<-' click={ () => this.onDeleteButtonClicked() } />
-        <Button val='AC' click={ () => this.onResetButtonClicked() } />
+        <Button val='<-' click={ () => this.onSpecialButtonClicked('del') } />
+        <Button val='AC' click={ () => this.onSpecialButtonClicked('AC') } />
         < br />
-        { this.state.buttons.map( (val, index) => {
-          return <Button key={ val } val={ val } click={ () => this.onButtonClicked(val) } />
+        { this.state.numPad.map( (val, index) => {
+          return <Button key={ val } val={ val } click={ () => this.onNumpadButtonClicked(val) } />
         } ) }
+        <br />
+        { this.state.operations.map( (val, index) => {
+          return <Button key={ val } val={ val } click={ () => this.onNumpadButtonClicked(val) } />
+        } ) }
+        <br />
+        <Button val='=' click={ () => this.onSpecialButtonClicked('=') } />
       </div>
     );
   }
